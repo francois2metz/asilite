0562de8d55379d00057f7f7cdfb8dfe236ce559e
François de Metz <francois@2metz.fr>
2018-01-15 00:06:08 +0100

    Add pagination.

--- lib/asitext_web/controllers/page_controller.ex
+++ lib/asitext_web/controllers/page_controller.ex
@@ -1,14 +1,16 @@
 defmodule AsitextWeb.PageController do
   use AsitextWeb, :controller
 
-  def index(conn, _params) do
-    results = get_asi(conn, "search").body
-    render conn, "index.html", title: "Accueil", results: results
+  def index(conn, params) do
+    start   = Map.get(params, "start", "0")
+    results = get_asi(conn, "search", %{}, ["Range": format_range(start)]).body
+    render conn, "index.html", title: "Accueil", results: results, start: start
   end
 
-  def type(conn, %{"type" => type}) do
-    results = get_asi(conn, "search", "format=" <> type).body
-    render conn, "type.html", title: type, results: results, type: type
+  def type(conn, %{"type" => type} = params) do
+    start   = Map.get(params, "start", "0")
+    results = get_asi(conn, "search", %{"format" => type}, ["Range": format_range(start)]).body
+    render conn, "type.html", title: type, results: results, type: type, start: start
   end
 
   def show(conn, %{"type" => type, "slug" => slug}) do
@@ -39,18 +41,26 @@ defmodule AsitextWeb.PageController do
     end
   end
 
+  def format_range(start) do
+    "objects "<> start <>" "<>  Integer.to_string(String.to_integer(start) + 10)
+  end
+
   def get_asi(conn, path) do
-    get_asi(conn, path, "")
+    get_asi(conn, path, %{})
   end
 
   def get_asi(conn, path, query) do
+    get_asi(conn, path, query, [])
+  end
+
+  def get_asi(conn, url, query, headers) do
     access_token = get_session(conn, :access_token)
-    url = case access_token do
-            nil ->
-              path
-            _ ->
-              path <>"?"<> query <>"&access_token=" <> access_token
-          end
-    Asi.get(url)
+    query2 = case access_token do
+               nil ->
+                 query
+               _ ->
+                 Map.put(query, "access_token", access_token)
+             end
+    Asi.get(url, query: query2, headers: headers)
   end
 end
