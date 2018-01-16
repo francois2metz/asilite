4e0f7d2c8c8ee3cf109d4179f53babf6d14c35b7
François de Metz <francois@2metz.fr>
2018-01-16 12:11:55 +0100

    Refactor how the requests are made.

--- lib/asitext_web/controllers/page_controller.ex
+++ lib/asitext_web/controllers/page_controller.ex
@@ -1,28 +1,31 @@
 defmodule AsitextWeb.PageController do
   use AsitextWeb, :controller
+  plug :get_user_data
 
   def index(conn, params) do
-    start             = Map.get(params, "start", "0")
-    {conn2, response} = get_asi(conn, "search", %{}, ["Range": format_range(start)])
+    start    = Map.get(params, "start", "0")
+    response = get_asi(conn, "search", %{}, ["Range": format_range(start)])
 
-    render conn2, "index.html", title: "Accueil", results: response.body, start: start
+    render conn, "index.html", title: "Accueil", results: response.body, start: start
   end
 
   def type(conn, %{"type" => type} = params) do
-    format            = case type do
-                          "chroniques" -> "chronique"
-                          "articles"   -> "article"
-                          "emissions"   -> "emission"
-                        end
-    start             = Map.get(params, "start", "0")
-    {conn2, response} = get_asi(conn, "search", %{"format" => format}, ["Range": format_range(start)])
-    render conn2, "type.html", title: String.capitalize(type), results: response.body, type: type, start: start
+    format   = case type do
+                 "chroniques" -> "chronique"
+                 "articles"   -> "article"
+                 "emissions"  -> "emission"
+               end
+    start    = Map.get(params, "start", "0")
+    response = get_asi(conn, "search", %{"format" => format}, ["Range": format_range(start)])
+
+    render conn, "type.html", title: String.capitalize(type), results: response.body, type: type, start: start
   end
 
   def show(conn, %{"type" => type, "slug" => slug}) do
-    {conn2, response} = get_asi(conn, "contents/" <> type <> "/" <> slug)
-    title             = response.body["title"]
-    render conn2, "show.html", article: response.body, title: title
+    response = get_asi(conn, "contents/" <> type <> "/" <> slug)
+    title    = response.body["title"]
+
+    render conn, "show.html", article: response.body, title: title
   end
 
   def login(conn, _params) do
@@ -46,12 +49,22 @@ defmodule AsitextWeb.PageController do
   end
 
   def atom(conn, _params) do
-    {conn2, response} = get_asi(conn, "search")
-    render conn2, "atom.xml", results: response.body
+    response = get_asi(conn, "search")
+
+    render conn, "atom.xml", results: response.body
   end
 
   def format_range(start) do
     "objects "<> start <>" "<>  Integer.to_string(String.to_integer(start) + 10)
+  end
+
+  def get_user_data(conn, _) do
+    case get_session(conn, :access_token) do
+      nil ->
+        assign(conn, :logged, false)
+      _ ->
+        assign(conn, :logged, true)
+    end
   end
 
   def get_asi(conn, path) do
@@ -71,31 +84,39 @@ defmodule AsitextWeb.PageController do
   end
 
   def get_asi(conn, url, query, headers, retry) do
-    access_token = get_session(conn, :access_token)
-    {conn2, query2} = case access_token do
-                        nil ->
-                          {assign(conn, :logged, false), query}
-                        _ ->
-                          {assign(conn, :logged, true), Map.put(query, "access_token", access_token)}
-                      end
-    response = Asi.get(url, query: query2, headers: headers)
+    response = Asi.get(url, query: set_query_params(conn, query), headers: headers)
 
     case response.status_code do
       401 ->
-        result = Asi.refresh_token(access_token, get_session(conn2, :refresh_token))
-        case result.status_code do
-          200 ->
-            body = Poison.decode!(result.body)
-            put_session(conn2, :access_token, body["access_token"]) |>
-              put_session(:refresh_token, body["refresh_token"]) |>
-              get_asi(url, query, headers, retry + 1)
-          _ ->
-            {put_flash(conn2, :error, "Could not connect"), response}
-        end
+        refresh_token(conn) |>
+          get_asi(url, query, headers, retry + 1)
+      _ ->
+        response
+    end
+  end
+
+  def refresh_token(conn) do
+    response = Asi.refresh_token(get_session(conn, :access_token), get_session(conn, :refresh_token))
+
+    case response.status_code do
       200 ->
-        {conn2, response}
-      206 ->
-        {conn2, response}
+        body = Poison.decode!(response.body)
+        put_session(conn, :access_token, body["access_token"]) |>
+          put_session(:refresh_token, body["refresh_token"])
+      _ ->
+        put_flash(conn, :error, "La session a expiré") |>
+          delete_session(:access_token) |>
+          delete_session(:refresh_token)
+    end
+  end
+
+  def set_query_params(conn, query) do
+    access_token = get_session(conn, :access_token)
+    case access_token do
+      nil ->
+        query
+      _ ->
+        Map.put(query, "access_token", access_token)
     end
   end
 end
