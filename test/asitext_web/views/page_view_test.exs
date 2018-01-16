ae330785d94729637a7a1bf20db8f378f789e82d
François de Metz <francois@2metz.fr>
2018-01-16 23:14:41 +0100

    Ad tests on title_cleanup.

--- test/asitext_web/views/page_view_test.exs
+++ test/asitext_web/views/page_view_test.exs
@@ -1,3 +1,11 @@
 defmodule AsitextWeb.PageViewTest do
   use AsitextWeb.ConnCase, async: true
+
+  test "title_cleanup with nil" do
+    assert AsitextWeb.PageView.title_cleanup(nil) == nil
+  end
+
+  test "title_cleanup replace &nbps;" do
+    assert AsitextWeb.PageView.title_cleanup("&nbsp;PLOP&nbsp;") == " PLOP "
+  end
 end
