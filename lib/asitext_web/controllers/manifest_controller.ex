eb61afeef397a5fb1df869993a7387632a09c80f
François de Metz <francois@2metz.fr>
2018-02-07 21:51:38 +0100

    Add more fields to the manifest.

--- lib/asitext_web/controllers/manifest_controller.ex
+++ lib/asitext_web/controllers/manifest_controller.ex
@@ -5,6 +5,10 @@ defmodule AsitextWeb.ManifestController do
     manifest = %{
       name: "ASI Lite",
       description: "Affiche les contenu de arretsurimages.net d'une manière optimisée sur mobile.",
+      short_name: "ASI Lite",
+      start_url: "/",
+      display: "standalone",
+      icons: []
     }
     render conn, manifest: manifest
   end
