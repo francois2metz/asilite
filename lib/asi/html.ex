b18ce81e214428a9a3721781d76b7ce84130f3a6
François de Metz <francois@2metz.fr>
2018-02-17 17:19:12 +0100

    Move html rewrite to it's own module.

---
+++ lib/asi/html.ex
@@ -0,0 +1,108 @@
+defmodule Asi.HTML do
+  def rewrite_html(html, fetch_content) do
+    html
+    |> Floki.parse()
+    |> rewrite_tags(fetch_content)
+    |> Floki.raw_html()
+  end
+
+  defp rewrite_tags(html, fetch_content) do
+    html
+    |> map(&rewrite_tag(&1, fetch_content))
+  end
+
+  defp rewrite_tag({"a", attributes, rest}, _) do
+    {attributes, href} = without_key(attributes, "href")
+
+    {"a", [{"href", rewrite_href(href)} | :proplists.delete("target", attributes)], rest}
+  end
+
+  defp rewrite_tag({"asi-image", attributes, _rest}, _) do
+    {attributes, slug} = without_key(attributes, "slug")
+    {attributes, class} = without_key(attributes, "class")
+
+    {
+      "div",
+      [{"class", "image "<> class} | attributes],
+      [
+        {
+          "img",
+          [
+            {"src", "https://api.arretsurimages.net/api/public/media/"<> slug <>"/action/show?format=public"},
+            {"alt", ""}
+          ],
+          []
+        }
+      ]
+    }
+  end
+
+  defp rewrite_tag({"asi-encadre", attributes, rest}, _) do
+    {attributes, class} = without_key(attributes, "class")
+    {"div", [{"class", "encadre "<> class} | attributes], rest}
+  end
+
+  defp rewrite_tag({"asi-html", attributes, rest}, _) do
+    {"div", attributes, rest}
+  end
+
+  defp rewrite_tag({"asi-citation", attributes, rest}, _) do
+    {attributes, class} = without_key(attributes, "class")
+    {"div", [{"class", "citation "<> class} | attributes], rest}
+  end
+
+  defp rewrite_tag({"asi-video", attributes, _rest}, fetch_content) do
+    {attributes, slug} = without_key(attributes, "slug")
+    response = fetch_content.(slug)
+    embed_url = response.body["metas"]["embed_url"]
+    {
+      "div",
+      [{"class", "embed-responsive embed-responsive-16by9"}],
+      [
+        {
+          "iframe",
+          [
+            {"src", embed_url},
+            {"allowfullscreen", "true"}
+            | attributes
+          ],
+          []
+        }
+      ]
+    }
+  end
+
+  defp rewrite_tag(other, _) do
+    other
+  end
+
+  defp rewrite_href(href) do
+    href
+    |> String.replace(~r/^\/([^\/]+)\/[0-9-]+\/(.+)/, "/\\1/\\2")
+    |> String.replace(~r/^\/([^\/]+)\/(.+)-id[0-9]+/, "/\\1/\\2")
+    |> String.replace(~r/https:\/\/beta.arretsurimages.net\/([^\/]+)\/[0-9-]+\/(.+)/, "/\\1/\\2")
+    |> String.replace(~r/https:\/\/beta.arretsurimages.net\/([^\/]+)\/([^\/]+)/, "/\\1/\\2")
+    |> String.replace(~r/^\/breves\/(.+)/, "/articles/\\1")
+    |> String.replace(~r/-l-([a-zA-Z]+)/, "-l\\1")
+    |> String.replace(~r/-d-([a-zA-Z]+)/, "-d\\1")
+    |> String.replace(~r/-n-([a-zA-Z]+)/, "-n\\1")
+    |> String.replace(~r/-qu-([a-zA-Z]+)/, "-qu\\1")
+  end
+
+  defp without_key(list, key) do
+    value = :proplists.get_value(key, list, "")
+    list = :proplists.delete(key, list)
+    {list, value}
+  end
+
+  def map(html, fun) when is_list(html) do
+    Enum.map(html, &map(&1, fun))
+  end
+  def map({name, attrs, rest}, fun) do
+    {new_name, new_attrs, new_rest} = fun.({name, attrs, rest})
+
+    {new_name, new_attrs, Enum.map(new_rest, &map(&1, fun))}
+  end
+
+  def map(other, _fun), do: other
+end
---
+++ test/asi/html_test.exs
@@ -0,0 +1,88 @@
+defmodule Asi.HTMLTest do
+  use ExUnit.Case, async: true
+
+  test "rewrite links new" do
+    html = ~s(<div><a href="https://beta.arretsurimages.net/articles/france-bleu">test</a></div>)
+    assert Asi.HTML.rewrite_html(html, fn -> :ok end) == ~s(<div><a href="/articles/france-bleu">test</a></div>)
+  end
+
+  test "rewrite old links" do
+    html = ~s(<div><a href="https://beta.arretsurimages.net/articles/2017-12-12/france-bleu">test</a></div>)
+    assert Asi.HTML.rewrite_html(html, fn -> :ok end) == ~s(<div><a href="/articles/france-bleu">test</a></div>)
+  end
+
+  test "rewrite absolute links" do
+    html = ~s(<div><a href="/articles/2017-12-12/france-bleu">test</a></div>)
+    assert Asi.HTML.rewrite_html(html, fn -> :ok end) == ~s(<div><a href="/articles/france-bleu">test</a></div>)
+  end
+
+  test "rewrite absolute links with id" do
+    html = ~s(<div><a href="/chroniques/2017-11-13/Des-defonces-des-cretins-et-des-causalites-id10318">test</a></div>)
+    assert Asi.HTML.rewrite_html(html, fn -> :ok end) == ~s(<div><a href="/chroniques/Des-defonces-des-cretins-et-des-causalites">test</a></div>)
+  end
+
+  test "rewrite old breves links" do
+    html = ~s(<div><a href="/breves/2017-11-13/Des-defonces-des-cretins-et-des-causalites-id10318">test</a></div>)
+    assert Asi.HTML.rewrite_html(html, fn -> :ok end) == ~s(<div><a href="/articles/Des-defonces-des-cretins-et-des-causalites">test</a></div>)
+  end
+
+  test "rewrite links with l'" do
+    html = ~s(<div><a href="/chroniques/2017-11-13/canal-test-l-enquete-l-enquete">test</a></div>)
+    assert Asi.HTML.rewrite_html(html, fn -> :ok end) == ~s(<div><a href="/chroniques/canal-test-lenquete-lenquete">test</a></div>)
+  end
+
+  test "rewrite links with d'" do
+    html = ~s(<div><a href="/chroniques/Reponse-d-un-boeuf-carotte-a-Cash-investigation">test</a></div>)
+    assert Asi.HTML.rewrite_html(html, fn -> :ok end) == ~s(<div><a href="/chroniques/Reponse-dun-boeuf-carotte-a-Cash-investigation">test</a></div>)
+  end
+
+  test "rewrite links with qu" do
+    html = ~s(<div><a href="/chroniques/Lobby-du-tabac-le-lievre-de-Cash-investigation-n-est-il-qu-un-lapin-bizarre">test</a></div>)
+    assert Asi.HTML.rewrite_html(html, fn -> :ok end) == ~s(<div><a href="/chroniques/Lobby-du-tabac-le-lievre-de-Cash-investigation-nest-il-quun-lapin-bizarre">test</a></div>)
+  end
+
+  test "remove target blank from links" do
+    html = ~s(<div><a href="/articles/2017-12-12/france-bleu" target="_blank">test</a></div>)
+    assert Asi.HTML.rewrite_html(html, fn -> :ok end) == ~s(<div><a href="/articles/france-bleu">test</a></div>)
+  end
+
+  test "dont rewrite no links" do
+    html = ~s(<div><a href="http://lemonde.fr/articles/2017-12-12/france-bleu">test</a></div>)
+    assert Asi.HTML.rewrite_html(html, fn -> :ok end) == ~s(<div><a href="http://lemonde.fr/articles/2017-12-12/france-bleu">test</a></div>)
+  end
+
+  test "rewrite asi-image to div" do
+    html = ~s(<div><asi-image class="no-remove" slug="test">Content</asi-image></div>)
+    assert Asi.HTML.rewrite_html(html, fn -> :ok end) == ~s(<div><div class="image no-remove"><img src="https://api.arretsurimages.net/api/public/media/test/action/show?format=public" alt=""/></div></div>)
+  end
+
+  test "support rewrite asi-image to div without class" do
+    html = ~s(<div><asi-image slug="test">Content</asi-image></div>)
+    assert Asi.HTML.rewrite_html(html, fn -> :ok end) == ~s(<div><div class="image "><img src="https://api.arretsurimages.net/api/public/media/test/action/show?format=public" alt=""/></div></div>)
+  end
+
+  test "rewrite asi-encadre to div" do
+    html = ~s(<div><asi-encadre class="no-remove">Content</asi-encadre></div>)
+    assert Asi.HTML.rewrite_html(html, fn -> :ok end) == ~s(<div><div class="encadre no-remove">Content</div></div>)
+  end
+
+  test "don't rewrite other div" do
+    html = ~s(<div><div class="no-remove">Content</div></div>)
+    assert Asi.HTML.rewrite_html(html, fn -> :ok end) == ~s(<div><div class="no-remove">Content</div></div>)
+  end
+
+  test "rewrite asi-video to use iframe" do
+    html = ~s(<div><asi-video class="no-remove" slug="my-video">Content</asi-video></div>)
+    assert Asi.HTML.rewrite_html(html, fn _ -> %{body: %{"metas" => %{"embed_url" => "https://exemple.net/my-video"}}} end) == ~s(<div><div class="embed-responsive embed-responsive-16by9"><iframe src="https://exemple.net/my-video" allowfullscreen="true" class="no-remove"></iframe></div></div>)
+  end
+
+  test "rewrite asi-citation to a div" do
+    html = ~s(<div><asi-citation class="no-remove">Content</asi-citation></div>)
+    assert Asi.HTML.rewrite_html(html, fn _ -> :ok end) == ~s(<div><div class="citation no-remove">Content</div></div>)
+  end
+
+  test "rewrite asi-html to a div" do
+    html = ~s(<div><asi-html>Content</asi-html></div>)
+    assert Asi.HTML.rewrite_html(html, fn _ -> :ok end) == ~s(<div><div>Content</div></div>)
+  end
+end
